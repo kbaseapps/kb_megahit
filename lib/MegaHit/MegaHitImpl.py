6cf29e8bb157a7d9510732d1b7675b5105d0952a
msneddon <mwsneddon@lbl.gov>
2015-11-20 11:06:20 -0800

    temporarily remove generation of r client

--- lib/MegaHit/MegaHitImpl.py
+++ lib/MegaHit/MegaHitImpl.py
@@ -279,20 +279,4 @@ This sample module contains one small method - count_contigs.
                              'output is not type dict as required.')
         # return the results
         return [output]
-
-    def count_contigs(self, ctx, workspace_name, contigset_id):
-        # ctx is the context object
-        # return variables are: returnVal
-        #BEGIN count_contigs
-        token = ctx['token']
-        wsClient = workspaceService(self.workspaceURL, token=token)
-        contigSet = wsClient.get_objects([{'ref': workspace_name+'/'+contigset_id}])[0]['data']
-        returnVal = {'contig_count': len(contigSet['contigs'])}
-        #END count_contigs
-
-        # At some point might do deeper type checking...
-        if not isinstance(returnVal, dict):
-            raise ValueError('Method count_contigs return value ' +
-                             'returnVal is not type dict as required.')
-        # return the results
-        return [returnVal]
+        
