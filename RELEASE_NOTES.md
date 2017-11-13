a2d3624decdfbead95e733d8c8971513b9a426d7
dcchivian <dcchivian@lbl.gov>
2017-11-13 12:17:23 -0800

    changed contact from email to url

--- RELEASE_NOTES.md
+++ RELEASE_NOTES.md
@@ -1,3 +1,7 @@
+### Version 2.2.8
+__Changes__
+- changed contact from email to url
+
 ### Version 2.2.7
 __Changes__
 - updated min_contig_len to min_contig_length (to be consistent with other MG assembler arg names) and set default to 2Kbp and min of 300bp (also to be consistent).
--- lib/MEGAHIT/MEGAHITImpl.py
+++ lib/MEGAHIT/MEGAHITImpl.py
@@ -34,8 +34,8 @@ class MEGAHIT:
     # state. A method could easily clobber the state set by another while
     # the latter method is running.
     ######################################### noqa
-    VERSION = "2.2.7"
-    GIT_URL = "https://github.com/dcchivian/kb_megahit"
+    VERSION = "2.2.8"
+    GIT_URL = "https://github.com/kbaseapps/kb_megahit"
     GIT_COMMIT_HASH = "0e8e31ce50ed58dfebd385b099571bb55b26c0e7"
 
     #BEGIN_CLASS_HEADER
